73fa6913c63cbd7996003e7967f093cd88a3e8fd
HatTapper <aidanmyles206@gmail.com>
2025-01-06 11:47:18 -0500

    Initial commit

---
+++ src/Week1/Welcome.java
@@ -0,0 +1,19 @@
+package Week1;
+
+// Project name: COP_2210
+// File name: Welcome
+// Class name: Welcome
+// Package name: Week1
+
+// the name of the public class should match the name of the file
+public class Welcome {
+    // this method is needed to run the program
+    public static void main(String[] args) {
+        // System.out.println() prints the given string to the console and then moves to the next line
+        System.out.println("Welcome to COP 2210!");
+
+        // System.out.print() prints the given string to the console and does not move to the next line
+        System.out.print("Aidan. ");
+        System.out.print("I love Java!");
+    }
+}
