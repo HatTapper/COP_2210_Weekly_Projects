b9f506c0c51ba7ce3eedd54aab0dc8e3a3e681a7
Relvos <anguyen57080@gmail.com>
2025-01-06 22:54:23 -0500

    push test

--- src/Week1/Task1.java
+++ src/Week1/Task1.java
@@ -6,6 +6,8 @@ public class Task1 {
         // this will simply print as-is
         System.out.print("1+2+3+4+5+6+7+8+9 = ");
 
+        // git push test
+
         // this will print the intended sum
         System.out.println(1+2+3+4+5+6+7+8+9);
     }
