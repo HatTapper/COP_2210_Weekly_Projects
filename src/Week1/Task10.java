81d0f39f2b6e85fab4c1d4f95e75258826635e5e
HatTapper <aidanmyles206@gmail.com>
2025-01-08 11:38:10 -0500

    added unit

--- src/Week1/Task10.java
+++ src/Week1/Task10.java
@@ -3,7 +3,7 @@ package Week1;
 public class Task10 {
     public static void main(String[] args) {
         // runner travels 14 km in 45 mins and 30 seconds, find average mph speed
-        // 1 mile = 1.6 km, thus 14 / 1.6 = 8.75 miles
+        // 1 mile = 1.6 km, thus 14km / 1.6 = 8.75 miles
         // 45 mins and 30 seconds is equivalent to 45.5 minutes
         // to convert to hours, do 45.5 / 60, which equals ~0.758
 
